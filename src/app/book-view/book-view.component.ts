68cde047f902812828405f16a5c90e4e634f8d8a
Yasir Hasan <yasir.hasan@scotiabank.com>
2017-08-24 14:41:54 -0400

    Add collapsable hover menu on left of book-view

--- src/app/book-view/book-view.component.ts
+++ src/app/book-view/book-view.component.ts
@@ -22,7 +22,6 @@ export class BookViewComponent implements OnInit {
 
   ngOnInit() {
 	this.route.params.subscribe((id) => this.id = id);
-	console.log(this.id)
 	this.dataService.getAlKafiContentName(this.id.id).then(content_title => this.content_title  = content_title );
 	this.dataService.getAlKafiContent(this.id.id, -1, -1, -1).then(content => this.content = content);
   }
@@ -32,7 +31,19 @@ export class BookViewComponent implements OnInit {
   
   ngAfterViewInit(): void {
 	  setTimeout(function() { 
-
+        $("#collapsable").hover(
+            function(){
+				
+				setTimeout(function () { $(".chapter-name").removeClass("nav-text-name-hidden") }, 500);
+                $(this).animate({ "width" : "300%"}, 'fast', function() { }); 
+            },
+            function(){
+				setTimeout(function () { $(".chapter-name").addClass("nav-text-name-hidden") }, 500);
+				
+                $(this).animate({ "width" : "100%" }, 50);
+            }
+        );                             
+    
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
@@ -56,6 +67,8 @@ $('a[href*="#"]')
         }, 750, function() {
           // Callback after animation
           // Must change focus!
+		  $(".chapter-name").addClass("nav-text-name-hidden")
+		  $("#collapsable").width("60%")
           var $target = $(target);
           $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
